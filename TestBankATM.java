2002edc0f07b59186fa1eca963906ed2be203640
Navinbala5 <bnavin28@gmail.com>
2017-05-24 13:18:31 +0530

    Update TestBankATM.java

--- TestBankATM.java
+++ TestBankATM.java
@@ -9,19 +9,19 @@ public static void main(String[] args) {
 		b1.withdraw(500);
 		b1.deposit(200);
 	}catch(Exception e) {
-		System.out.println("Exception occurred "+e);
+		System.out.println("Exception has occurred "+e);
 	}
 	try {
 		b2.withdraw(600);
 		b2.deposit(300);
 	}catch(Exception e) {
-		System.out.println("Exception occurred "+e);
+		System.out.println("Exception has occurred "+e);
 	}
 	try {
 		b3.withdraw(11300);
 		b3.deposit(1700);
 	}catch(Exception e) {
-		System.out.println("Exception occurred "+e);
+		System.out.println("Exception has occurred "+e);
 	}
 }
 }
