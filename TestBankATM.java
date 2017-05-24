674297a772573f38f9471fbcd01bab154e059211
Navinbala5 <bnavin28@gmail.com>
2017-05-24 13:16:25 +0530

    Update TestBankATM.java

--- TestBankATM.java
+++ TestBankATM.java
@@ -6,20 +6,20 @@ public static void main(String[] args) {
 	BankATM b2=new BankATM(1000, "state bank", "chennai", 10000);
 	BankATM b3=new BankATM(1000, "state bank", "chennai", 10000);
 	try {
-		b1.withdraw(400);
-		b1.deposit(300);
+		b1.withdraw(500);
+		b1.deposit(200);
 	}catch(Exception e) {
 		System.out.println("Exception occurred "+e);
 	}
 	try {
-		b2.withdraw(700);
-		b2.deposit(200);
+		b2.withdraw(600);
+		b2.deposit(300);
 	}catch(Exception e) {
 		System.out.println("Exception occurred "+e);
 	}
 	try {
-		b3.withdraw(12500);
-		b3.deposit(500);
+		b3.withdraw(11300);
+		b3.deposit(1700);
 	}catch(Exception e) {
 		System.out.println("Exception occurred "+e);
 	}
